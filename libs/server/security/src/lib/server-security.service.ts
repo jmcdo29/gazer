850267c66366bd38cc6891ec8c63dc348b09b37a
Jay McDoniel <jmcdo29@gmail.com>
2023-09-20 15:42:14 -0700

    chore: just a bit of clean up, resetting the session expire and better back button

--- libs/server/security/src/lib/server-security.service.ts
+++ libs/server/security/src/lib/server-security.service.ts
@@ -33,7 +33,7 @@ export class ServerSecurityService {
           type: 'session',
           value: sessionToken,
           userId: user.id,
-          expiresAt: sql`NOW() + '1 MIN'`,
+          expiresAt: sql`NOW() + '1 DAY'`,
         },
         {
           type: 'refresh',
